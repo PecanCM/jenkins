aea92b0b03966b3a8583535c24e121d133e81cc3
Keyur Maru <keyur2maru@gmail.com>
2013-01-20 16:27:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -81,9 +81,6 @@ fi
 git config --global user.name $(whoami)@$NODE_NAME
 git config --global user.email cm.pecan@yahoo.com
 
-if [[ "$REPO_BRANCH" =~ "jellybean" || $REPO_BRANCH =~ "cm-10" ]]; then 
-   JENKINS_BUILD_DIR=jellybean
-else
    JENKINS_BUILD_DIR=$REPO_BRANCH
 fi
 
